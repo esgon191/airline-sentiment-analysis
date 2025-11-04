ca30f31d490d038bb3205875c7f03dd9e44688ce
Тимур Кафтаранов <kaftaranov.work@gmail.com>
2025-11-04 09:49:17 +0300

    Реализация тестового сообщения в логгер через параметры argparse

--- clearml_tests.py
+++ clearml_tests.py
@@ -1,5 +1,21 @@
 from clearml import Task, Logger
 from datetime import datetime
+import argparse
+
+# Тестирование работы clearml с парсингом аргументов
+parser = argparse.ArgumentParser(description="Пример: получение текста из аргументов командной строки")
+
+parser.add_argument(
+    "--test_text",
+    type=str,
+    required=True,
+    help="Текст для теста (пример: --test_text 'Привет, мир!')"
+)
+
+args = parser.parse_args()
+
+test_text = args.test_text
+
 
 try:
     task = Task.init(
@@ -9,6 +25,7 @@ try:
         output_uri=False
     )
     Logger.current_logger().report_text(f"Test task initialized successfully {datetime.now()}")
+    Logger.current_logger().report_text(f"Сообщение из аргументов: {test_text}")
     print("Успешно подключено к ClearML")
 
 except Exception as e:
