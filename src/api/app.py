501449fc70431c3468362802cb8ed70aad4383ee
Тимур Кафтаранов <kaftaranov.work@gmail.com>
2025-12-05 00:50:28 +0300

    load_model_in_app

--- src/api/app.py
+++ src/api/app.py
@@ -4,20 +4,36 @@ from fastapi import FastAPI, HTTPException
 from src.api.schemas import SentimentRequest
 from src.models.infer import SentimentInferenceModel
 
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    pipeline,
+)
+
+
 app = FastAPI(
     title="Airline Sentiment Analysis API",
     description="API предсказания тональности отзывов positive / neutral / negative",
     version="0.2.0",
 )
 
-model: SentimentInferenceModel | None = None
-
+pipe : pipeline | None = None
 
 @app.on_event("startup")
 def load_model() -> None:
-    global model
+    global pipe
     try:
-        model = SentimentInferenceModel.from_config("configs/inference.yaml")
+        tokenizer = AutoTokenizer.from_pretrained('local_models/')
+        model = AutoModelForSequenceClassification.from_pretrained('local_models/')
+
+        # return_all_scores=True → вероятности по всем классам тональности
+        clf = pipeline(
+            "text-classification",
+            model=model,
+            tokenizer=tokenizer,
+            return_all_scores=True,
+        )
+        
     except Exception as exc:  # noqa: BLE001
         print(f"Failed to load model: {exc}")
         model = None
@@ -25,22 +41,22 @@ def load_model() -> None:
 
 @app.get("/health")
 def health() -> dict:
-    status = "200 OK" if model is not None else "model_not_loaded"
+    status = "200 OK" if pipe is not None else "model_not_loaded"
     return {"status": status}
 
 
 @app.post("/predict")
 def predict(request: SentimentRequest):
-    model = getattr(app.state, "model", None)
+    pipe = getattr(app.state, "pipe", None)
 
-    if model is None:
+    if pipe is None:
         # модель не загружена – значит сервис недоступен
         raise HTTPException(
             status_code=503,
             detail="Model is not loaded. Cannot perform prediction.",
         )
     try:
-        result = model.predict([request.text])[0]
+        result = pipe(request.text)
         return result
     except Exception as exc:  # noqa: BLE001
         raise HTTPException(status_code=500, detail=str(exc)) from exc
